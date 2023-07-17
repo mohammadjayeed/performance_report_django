7f3255518f867b5a30aa526d7008369470994527
Mohammad Jayeed <thisisjayeed@gmail.com>
2023-07-18 03:38:58 +0600

    added crispy

--- performance_report/settings.py
+++ performance_report/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'reports',
     'products',
     'areas',
-    'category'
+    'category',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +120,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
